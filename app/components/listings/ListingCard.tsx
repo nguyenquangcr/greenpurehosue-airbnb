28ccdc63d15367f072fb760535ab1f4af2f7ceb8
15110108@student.hcmute.edu.vn <15110108@student.hcmute.edu.vn>
2023-11-25 11:28:55 +0700

    Update code fix bug

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -11,12 +11,13 @@ import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
-
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
+import Slider from "react-slick";
 //css
 import "./index.css";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 interface ListingCardProps {
   data: SafeListing;
   reservation?: SafeReservation;
@@ -74,6 +75,15 @@ const ListingCard: React.FC<ListingCardProps> = ({
   }, [reservation]);
 
   // group-hover:scale-110
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
@@ -89,12 +99,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
             rounded-xl
           "
         >
-          <Carousel
-            showDots={false}
-            additionalTransfrom={0}
-            arrows
-            autoPlaySpeed={3000}
-            centerMode={false}
+          <div
             className="
                 label-listing
                 object-cover 
@@ -102,65 +107,25 @@ const ListingCard: React.FC<ListingCardProps> = ({
                 w-full 
                 transition
               "
-            containerClass="container"
-            dotListClass=""
-            draggable
-            focusOnSelect={false}
-            infinite
-            itemClass=""
-            keyBoardControl
-            minimumTouchDrag={80}
-            pauseOnHover
-            renderArrowsWhenDisabled={false}
-            renderButtonGroupOutside={false}
-            renderDotsOutside={false}
-            responsive={{
-              desktop: {
-                breakpoint: {
-                  max: 3000,
-                  min: 1024,
-                },
-                items: 1,
-              },
-              mobile: {
-                breakpoint: {
-                  max: 464,
-                  min: 0,
-                },
-                items: 1,
-              },
-              tablet: {
-                breakpoint: {
-                  max: 1024,
-                  min: 464,
-                },
-                items: 1,
-              },
-            }}
-            rewind={false}
-            rewindWithAnimation={false}
-            rtl={false}
-            shouldResetAutoplay
-            sliderClass=""
-            slidesToSlide={1}
-            swipeable
           >
-            {data.imageSrc?.map((item, index) => {
-              return (
-                <img
-                  className="
+            <Slider {...settings}>
+              {data.imageSrc?.map((item, index) => {
+                return (
+                  <img
+                    className="
                 object-cover 
                 h-full 
                 w-full 
                 transition
                 "
-                  key={index}
-                  src={item}
-                  alt="Listing"
-                />
-              );
-            })}
-          </Carousel>
+                    key={index}
+                    src={item}
+                    alt="Listing"
+                  />
+                );
+              })}
+            </Slider>
+          </div>
           {/* <div>
             <Carousel
               showThumbs={false}
